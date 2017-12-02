2756d33f5b7eb634f4bb006e294053ee7f7beb0d
Will Storey <will@summercat.com>
2017-12-01 18:45:31 -0800

    Copy Makefile.mk and lib from 2016

---
+++ 2017/Makefile.mk
@@ -0,0 +1,61 @@
+CC=gcc
+
+# Reviewed warnings for gcc 6.2.1
+CFLAGS = \
+	-std=c11 -g -ggdb -pedantic -pedantic-errors \
+	-Werror -Wall -Wextra \
+	-Wformat=2 \
+	-Wformat-signedness \
+	-Wnull-dereference \
+	-Winit-self \
+	-Wmissing-include-dirs \
+	-Wshift-overflow=2 \
+	-Wswitch-default \
+	-Wswitch-enum \
+	-Wunused-const-variable=2 \
+	-Wuninitialized \
+	-Wunknown-pragmas \
+	-Wstrict-overflow=5 \
+	-Wsuggest-attribute=pure \
+	-Wsuggest-attribute=const \
+	-Wsuggest-attribute=noreturn \
+	-Wsuggest-attribute=format \
+	-Warray-bounds=2 \
+	-Wduplicated-cond \
+	-Wfloat-equal \
+	-Wundef \
+	-Wshadow \
+	-Wbad-function-cast \
+	-Wcast-qual \
+	-Wcast-align \
+	-Wwrite-strings \
+	-Wconversion \
+	-Wjump-misses-init \
+	-Wlogical-op \
+	-Waggregate-return \
+	-Wcast-align \
+	-Wstrict-prototypes \
+	-Wold-style-definition \
+	-Wmissing-prototypes \
+	-Wmissing-declarations \
+	-Wpacked \
+	-Wredundant-decls \
+	-Wnested-externs \
+	-Winline \
+	-Winvalid-pch \
+	-Wstack-protector
+
+# Assume all .c files correspond to a program to build.
+TARGETS_ALL=$(patsubst %.c,%,$(wildcard *.c))
+TARGETS=$(filter-out siphash24, $(TARGETS_ALL))
+
+all: $(TARGETS)
+
+# This is a static pattern rule to build all .c programs (TARGETS).
+# Don't define a recipe. Instead, rely on implicit rules to build these. If we
+# need to give more specific recipes, we can override the implicit rule by
+# writing a recipe for the target.
+$(TARGETS): %: %.c
+
+clean:
+	rm -f $(TARGETS)
---
+++ 2017/lib/hash.h
@@ -0,0 +1,7 @@
+#ifndef _HASH_H
+#define _HASH_H
+
+unsigned char *
+md5(const char * const);
+
+#endif

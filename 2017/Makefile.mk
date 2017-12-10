1714e815ad3ab1c5ce0adb1fcec40f75096245f0
Will Storey <will@summercat.com>
2017-12-10 10:32:02 -0800

    Update gcc warning flags for newer gcc version

--- 2017/Makefile.mk
+++ 2017/Makefile.mk
@@ -1,9 +1,11 @@
 CC=gcc
 
-# Reviewed warnings for gcc 6.2.1
+# Reviewed warnings for gcc 6.3.0
 CFLAGS = \
 	-std=c11 -g -ggdb -pedantic -pedantic-errors \
 	-Werror -Wall -Wextra \
+	-Wchkp \
+	-Wdouble-promotion \
 	-Wformat=2 \
 	-Wformat-signedness \
 	-Wnull-dereference \
@@ -12,6 +14,7 @@ CFLAGS = \
 	-Wshift-overflow=2 \
 	-Wswitch-default \
 	-Wswitch-enum \
+	-Wunused-parameter \
 	-Wunused-const-variable=2 \
 	-Wuninitialized \
 	-Wunknown-pragmas \
@@ -30,15 +33,16 @@ CFLAGS = \
 	-Wcast-align \
 	-Wwrite-strings \
 	-Wconversion \
+	-Wdate-time \
 	-Wjump-misses-init \
 	-Wlogical-op \
 	-Waggregate-return \
-	-Wcast-align \
 	-Wstrict-prototypes \
 	-Wold-style-definition \
 	-Wmissing-prototypes \
 	-Wmissing-declarations \
 	-Wpacked \
+	-Wpadded \
 	-Wredundant-decls \
 	-Wnested-externs \
 	-Winline \

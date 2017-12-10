ebc8108d11b6bc64e1485e78d331b260fd4d80d6
Will Storey <will@summercat.com>
2017-12-10 11:30:09 -0800

    Separate makefile into problems and lib
    This lets us simplify the problem makefiles

--- 2017/Makefile.mk
+++ 2017/Makefile.mk
@@ -1,3 +1,5 @@
+# This Makefile is suitable for including everywhere (problems and lib).
+
 CC=gcc
 
 # Reviewed warnings for gcc 6.3.0
@@ -47,18 +49,3 @@ CFLAGS = \
 	-Winline \
 	-Winvalid-pch \
 	-Wstack-protector
-
-# Assume all .c files correspond to a program to build.
-TARGETS_ALL=$(patsubst %.c,%,$(wildcard *.c))
-TARGETS=$(filter-out siphash, $(TARGETS_ALL))
-
-all: $(TARGETS)
-
-# This is a static pattern rule to build all .c programs (TARGETS).
-# Don't define a recipe. Instead, rely on implicit rules to build these. If we
-# need to give more specific recipes, we can override the implicit rule by
-# writing a recipe for the target.
-$(TARGETS): %: %.c
-
-clean:
-	rm -f $(TARGETS) *.o
---
+++ 2017/problems.mk
@@ -0,0 +1,28 @@
+# This Makefile is suitable for including in problem directories.
+#
+# It makes some assumptions that are not suitable for the lib directory.
+
+include ../Makefile.mk
+
+CFLAGS+=-I../lib
+LDFLAGS=-L../lib -ladvent -lssl -lcrypto
+
+# Assume all .c files correspond to a program to build.
+TARGETS_ALL=$(patsubst %.c,%,$(wildcard *.c))
+TARGETS=$(filter-out siphash, $(TARGETS_ALL))
+
+.PHONY: test
+
+all: $(TARGETS)
+
+# This is a static pattern rule to build all .c programs (TARGETS).
+# Don't define a recipe. Instead, rely on implicit rules to build these. If we
+# need to give more specific recipes, we can override the implicit rule by
+# writing a recipe for the target.
+$(TARGETS): %: %.c ../lib/libadvent.a
+
+../lib/libadvent.a:
+	$(MAKE) -C ../lib libadvent.a
+
+clean:
+	rm -f $(TARGETS)

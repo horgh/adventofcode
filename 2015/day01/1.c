8eccb6582606dc32027537c50e5214069cf7be12
Will Storey <will@summercat.com>
2018-12-01 12:50:28 -0800

    Add 2015 day 1

---
+++ 2015/day01/1.c
@@ -0,0 +1,29 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <assert.h>
+#include <stdio.h>
+
+int
+main(int const argc, char const * const * const argv)
+{
+	(void) argc;
+	(void) argv;
+
+	char buf[8192] = {0};
+	assert(fgets(buf, 8192, stdin) != NULL);
+
+	char const * ptr = buf;
+	int floor = 0;
+	while (*ptr == '(' || *ptr == ')') {
+		if (*ptr == '(') {
+			floor++;
+			ptr++;
+			continue;
+		}
+		floor--;
+		ptr++;
+	}
+
+	printf("%d\n", floor);
+	return 0;
+}
---
+++ 2015/day01/2.c
@@ -0,0 +1,36 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <assert.h>
+#include <stdio.h>
+
+int
+main(int const argc, char const * const * const argv)
+{
+	(void) argc;
+	(void) argv;
+
+	char buf[8192] = {0};
+	assert(fgets(buf, 8192, stdin) != NULL);
+
+	char const * ptr = buf;
+	int floor = 0;
+	int i = 0;
+	while (*ptr == '(' || *ptr == ')') {
+		i++;
+
+		if (*ptr == '(') {
+			floor++;
+			ptr++;
+			continue;
+		}
+		floor--;
+		ptr++;
+
+		if (floor == -1) {
+			break;
+		}
+	}
+
+	printf("%d\n", i);
+	return 0;
+}

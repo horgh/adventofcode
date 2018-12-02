4bf3ed13b6b0f6c78e7888041ee94bf997a770ed
Will Storey <will@summercat.com>
2018-12-01 20:25:06 -0800

    Add 2015 day 3

---
+++ 2015/day03/1.c
@@ -0,0 +1,53 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <assert.h>
+#include <map.h>
+#include <stdio.h>
+#include <string.h>
+
+int
+main(int const argc, char const * const * const argv)
+{
+	(void) argc;
+	(void) argv;
+
+	char buf[8192] = {0};
+	assert(fgets(buf, 8192, stdin) != NULL);
+
+	struct htable * const h = hash_init(1024);
+	assert(h != NULL);
+
+	int x = 0;
+	int y = 0;
+	assert(hash_set(h, "0,0", NULL));
+
+	char const * ptr = buf;
+	while (*ptr != '\0' && *ptr != '\n') {
+		switch (*ptr) {
+		case '^':
+			y++;
+			break;
+		case 'v':
+			y--;
+			break;
+		case '>':
+			x++;
+			break;
+		case '<':
+			x--;
+			break;
+		default:
+			assert(1 == 0);
+		}
+		ptr++;
+
+		char s[128] = {0};
+		sprintf(s, "%d,%d", x, y);
+		assert(hash_set(h, s, NULL));
+	}
+
+	int const houses = hash_count_elements(h);
+	printf("%d\n", houses);
+	assert(hash_free(h, NULL));
+	return 0;
+}
---
+++ 2015/day03/2.c
@@ -0,0 +1,78 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <assert.h>
+#include <map.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+int
+main(int const argc, char const * const * const argv)
+{
+	(void) argc;
+	(void) argv;
+
+	char buf[8192] = {0};
+	assert(fgets(buf, 8192, stdin) != NULL);
+
+	struct htable * const h = hash_init(1024);
+	assert(h != NULL);
+
+	int santa_x = 0;
+	int santa_y = 0;
+	int robo_santa_x = 0;
+	int robo_santa_y = 0;
+	assert(hash_set(h, "0,0", NULL));
+
+	char const * ptr = buf;
+	bool santa = true;
+	while (*ptr != '\0' && *ptr != '\n') {
+		switch (*ptr) {
+		case '^':
+			if (santa) {
+				santa_y++;
+			} else {
+				robo_santa_y++;
+			}
+			break;
+		case 'v':
+			if (santa) {
+				santa_y--;
+			} else {
+				robo_santa_y--;
+			}
+			break;
+		case '>':
+			if (santa) {
+				santa_x++;
+			} else {
+				robo_santa_x++;
+			}
+			break;
+		case '<':
+			if (santa) {
+				santa_x--;
+			} else {
+				robo_santa_x--;
+			}
+			break;
+		default:
+			assert(1 == 0);
+		}
+		ptr++;
+
+		char s[128] = {0};
+		if (santa) {
+			sprintf(s, "%d,%d", santa_x, santa_y);
+		} else {
+			sprintf(s, "%d,%d", robo_santa_x, robo_santa_y);
+		}
+		santa = !santa;
+		assert(hash_set(h, s, NULL));
+	}
+
+	int const houses = hash_count_elements(h);
+	printf("%d\n", houses);
+	assert(hash_free(h, NULL));
+	return 0;
+}

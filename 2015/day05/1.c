c35b8479400a40117c18471bc27e58c3362139c9
Will Storey <will@summercat.com>
2018-12-02 16:40:25 -0800

    Add 2015 day 5

---
+++ 2015/day05/1.c
@@ -0,0 +1,64 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <util.h>
+
+static bool
+is_nice(char const * const);
+
+int
+main(int const argc, char const * const * const argv)
+{
+	(void) argc;
+	(void) argv;
+
+	int count = 0;
+	while (1) {
+		char buf[8192] = {0};
+		if (fgets(buf, 8192, stdin) == NULL) {
+			break;
+		}
+		trim_right(buf);
+
+		if (is_nice(buf)) {
+			count++;
+		}
+	}
+
+	printf("%d\n", count);
+	return 0;
+}
+
+__attribute__((pure))
+static bool
+is_nice(char const * const s)
+{
+	int vowel_count = 0;
+	bool has_two_in_a_row = false;
+	bool has_bad_string = false;
+	char const * ptr = s;
+	while (*ptr != '\0') {
+		if (*ptr == 'a' || *ptr == 'e' || *ptr == 'i' || *ptr == 'o' ||
+				*ptr == 'u') {
+			vowel_count++;
+		}
+
+		char const next_char = *(ptr+1);
+
+		if (*ptr == next_char) {
+			has_two_in_a_row = true;
+		}
+
+		if ((*ptr == 'a' && next_char == 'b') ||
+				(*ptr == 'c' && next_char == 'd') ||
+				(*ptr == 'p' && next_char == 'q') ||
+				(*ptr == 'x' && next_char == 'y')) {
+			has_bad_string = true;
+		}
+		ptr++;
+	}
+
+	return vowel_count >= 3 && has_two_in_a_row && !has_bad_string;
+}
---
+++ 2015/day05/2.c
@@ -0,0 +1,61 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <util.h>
+
+static bool
+is_nice(char const * const);
+
+int
+main(int const argc, char const * const * const argv)
+{
+	(void) argc;
+	(void) argv;
+
+	int count = 0;
+	while (1) {
+		char buf[8192] = {0};
+		if (fgets(buf, 8192, stdin) == NULL) {
+			break;
+		}
+		trim_right(buf);
+
+		if (is_nice(buf)) {
+			count++;
+		}
+	}
+
+	printf("%d\n", count);
+	return 0;
+}
+
+__attribute__((pure))
+static bool
+is_nice(char const * const s)
+{
+	bool has_pair = false;
+	bool has_same_separated_by_one = false;
+	char const * ptr = s;
+	while (*ptr != '\0') {
+		if (*(ptr+1) != '\0') {
+			char buf[3] = {0};
+			buf[0] = *ptr;
+			buf[1] = *(ptr+1);
+			if (strstr(ptr+2, buf) != NULL) {
+				has_pair = true;
+			}
+		}
+
+		if (*(ptr+1) != '\0' && *(ptr+2) != '\0') {
+			if (*ptr != *(ptr+1) && *ptr == *(ptr+2)) {
+				has_same_separated_by_one = true;
+			}
+		}
+
+		ptr++;
+	}
+
+	return has_pair && has_same_separated_by_one;
+}
